0d1f9005e15119777e084e15af6db953ecc62c03
Adilet <akunov313131@gmail.com>
2023-07-28 16:07:11 +0600

    sfdsdgsdgs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import Pricing from "./components/Pricing";
-import Header from "./components/Header";
 import {Content} from "./components/Content";
 
 function App() {
